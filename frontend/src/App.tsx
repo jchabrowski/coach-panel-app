10e8c11ac1ad0094c75ee1c913fab8b439086ad0
jchabrowski <chabrowski.kuba@gmail.com>
2023-09-21 11:00:40 +0200

    #1 Add basic eslint and prettier config, .gitignore

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,5 @@
 function App() {
-  return (
-      <h1 className="text-2xl font-bold underline bg-green-300">
-        Hello world!
-      </h1>
-  )
+  return <h1 className="text-2xl font-bold underline bg-green-300">Hello world!</h1>;
 }
 
-export default App
+export default App;
